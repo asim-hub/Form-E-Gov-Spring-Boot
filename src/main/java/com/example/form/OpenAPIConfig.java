10776f764d309b3cde7e901eaffa1f8379f029e5
adelin232 <narcis.adelin.miulet@gmail.com>
2023-01-08 23:13:21 +0200

    Add OpenAPIConfig

---
+++ src/main/java/com/example/form/OpenAPIConfig.java
@@ -0,0 +1,15 @@
+package com.example.form;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "weatherbackend",
+                version = "v2.0"
+        ),
+        servers = @Server(url = "http://formbackend:8080")
+)
+public class OpenAPIConfig {
+}
